25efd1cc398705d0d644c69677bab4479cdfb8fc
akifo <akiho@bb07.net>
2023-11-29 10:10:39 +0900

    transition appear の実験

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import MyAppear from "./components/MyAppear.vue";
+import NormalTransition from "./components/NormalTransition.vue";
 </script>
 
 <template>
   <HelloWorld msg="Vite + Vue" />
+  <MyAppear />
+  <hr />
+  <NormalTransition />
 </template>
---
+++ src/components/MyAppear.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const showWelcomeMessage = ref(true);
+</script>
+
+<template>
+  <button @click="showWelcomeMessage = !showWelcomeMessage">
+    Toggle Welcome Message
+  </button>
+  <transition appear>
+    <div v-if="showWelcomeMessage">Welcome to our website!</div>
+  </transition>
+</template>
+
+<style scoped>
+/* トランジションの開始状態 */
+.v-enter-from,
+.v-appear-from {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+
+/* トランジションの終了状態 */
+.v-enter-to,
+.v-appear-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+/* トランジションの期間 */
+.v-enter-active,
+.v-appear-active {
+  transition: opacity 1.5s, transform 1.5s;
+}
+</style>
